5f837211ff8a041762996992875637ddea0a99db
Amber Spadafora <ahelenespadafora@gmail.com>
2021-01-16 00:25:36 -0500

    Initial Commit

---
+++ day60Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day60Challenge
+//
+//  Created by Amber Spadafora on 1/16/21.
+//  Copyright © 2021 Amber Spadafora. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
